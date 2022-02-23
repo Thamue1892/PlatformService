4c9322d3a578a89164c003f1c706fa968bd5a988
ThaymesCoder <thamue1892@gmail.com>
2022-02-23 15:07:16 +0200

    Added Platform Controller
    Added PlatformController and implemented 3 endpoints.

--- Startup.cs
+++ Startup.cs
@@ -31,13 +31,14 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMem"));
 
-      services.AddScoped<IPlatformRepo, PlatformRepo>();
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
 
-      services.AddControllers();
+            services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
-            });
+                    {
+                        c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
